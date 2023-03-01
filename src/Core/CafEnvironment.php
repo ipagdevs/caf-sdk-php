672fd7a5d9040907035d09018fb43136353cffbd
Daniel Ishibashi <daniel@ipag.com.br>
2023-03-01 18:04:43 -0300

    ajustado nome composer.json

--- src/Core/CafEnvironment.php
+++ src/Core/CafEnvironment.php
@@ -8,6 +8,6 @@ class CafEnvironment extends Environment
 {
     public function __construct()
     {
-        parent::__construct('https://api.beta.combateafraude.com');
+        parent::__construct('https://api.combateafraude.com');
     }
 }
